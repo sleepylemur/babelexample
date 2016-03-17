30619390dd6ba7af7140fb6464c2a8eb53c13432
Evan Griffiths <griffithse@gmail.com>
2016-03-17 14:37:14 -0400

    fix some testing stuff and dependencies

---
+++ README.md
@@ -0,0 +1,27 @@
+Minimal example of using es6 with watch, minification, linting, and tests.
+
+### install
+
+```
+npm install
+```
+
+### local development
+```
+npm run watch
+```
+
+### run server
+```
+npm start
+```
+
+### minified build
+```
+npm run build
+```
+
+### tests
+```
+npm test
+```
--- test/bing.js
+++ test/bing.js
@@ -1,9 +1,10 @@
 /* eslint-env mocha */
-import greeting from '../src/bing.js';
 const expect = require('chai').expect;
+
+import greeting from '../src/bing.js';
 
 describe('bing', () => {
   it('returns bing', () => {
-    expect(greeting()).to.equal('bing');
+    expect(greeting()).to.equal('bing!');
   });
 });
